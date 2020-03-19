ca11e54e20d8c17ec46e7312aed4f9ea2f8bbf12
Marco Uberti <marcouberti84@gmail.com>
2020-03-19 15:06:10 +0100

    feat: notifications callback.

--- app/src/main/java/com/bendingspoons/ascolto/AscoltoTheirsConfiguration.kt
+++ app/src/main/java/com/bendingspoons/ascolto/AscoltoTheirsConfiguration.kt
@@ -1,5 +1,6 @@
 package com.bendingspoons.ascolto
 
+import android.util.Log
 import com.bendingspoons.concierge.ConciergeManager
 import com.bendingspoons.pico.Pico
 import com.bendingspoons.theirs.TheirsConfiguration
@@ -7,6 +8,7 @@ import com.bendingspoons.theirs.adjust.AdjustAppSecret
 import com.bendingspoons.theirs.adjust.AdjustConfiguration
 import com.bendingspoons.theirs.adjust.AdjustEnvironment
 import com.bendingspoons.theirs.fcm.FirebaseFCMConfiguration
+import com.google.firebase.messaging.RemoteMessage
 import org.koin.core.KoinComponent
 import org.koin.core.inject
 
@@ -20,5 +22,15 @@ class AscoltoTheirsConfiguration: TheirsConfiguration, KoinComponent {
     }
     override fun firebaseFCMConfig() = object: FirebaseFCMConfiguration {
         override val concierge: ConciergeManager by inject()
+
+        override suspend fun onNewPushNotification(remoteMessage: RemoteMessage) {
+            // Check if message contains a data payload.
+            //toast("DATA ${remoteMessage.data}.")
+
+            // Check if message contains a notification payload.
+            //remoteMessage.notification?.let {
+            //    toast("NOTIFICATION ${it.body}")
+            //}
+        }
     }
 }
