affa37130a9e3ffb7681c574e408a6c9653667b8
Matteo Battaglio <m4dbat@gmail.com>
2020-03-20 16:32:39 +0100

    Fix SurveyUnitTest's json

--- app/src/test/java/com/bendingspoons/ascolto/SurveyUnitTests.kt
+++ app/src/test/java/com/bendingspoons/ascolto/SurveyUnitTests.kt
@@ -12,6 +12,7 @@ class SurveyUnitTests {
     val surveyJson: String = """
         {
             "version": "20200319",
+            "logic_version": "20200319",
             "questions": [
                 {
                     "id": "question1",
