d819e550d91bdeabc61c2b79ec0bea7661864d0c
Marco Uberti <marcouberti84@gmail.com>
2020-03-22 11:58:54 +0100

    chore: update sesame key.

--- app/src/main/java/org/ascolto/onlus/geocrowd19/android/OracleSesameConfiguration.kt
+++ app/src/main/java/org/ascolto/onlus/geocrowd19/android/OracleSesameConfiguration.kt
@@ -3,5 +3,5 @@ package org.ascolto.onlus.geocrowd19.android
 import com.bendingspoons.sesame.SesameConfiguration
 
 class OracleSesameConfiguration: SesameConfiguration {
-    override val secretKey = "8K6aPPgXdF46yNw9wQZm9s7eK23ALr8z"
+    override val secretKey = "yfqf7tkVYvEba33ngrjP4GQPracpZXnk"
 }
