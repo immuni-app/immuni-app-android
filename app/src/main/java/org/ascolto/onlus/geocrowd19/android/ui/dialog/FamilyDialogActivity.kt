aedee1474b7de09ad72c036870a431871668c431
Marco Uberti <marcouberti84@gmail.com>
2020-03-25 01:14:01 +0100

    fix: add link to add family dialog.

--- app/src/main/java/org/ascolto/onlus/geocrowd19/android/ui/dialog/FamilyDialogActivity.kt
+++ app/src/main/java/org/ascolto/onlus/geocrowd19/android/ui/dialog/FamilyDialogActivity.kt
@@ -1,12 +1,15 @@
 package org.ascolto.onlus.geocrowd19.android.ui.dialog
 
+import android.content.Intent
 import android.os.Bundle
 import com.bendingspoons.base.extensions.setDarkStatusBarFullscreen
 import com.bendingspoons.base.extensions.setLightStatusBarFullscreen
 import kotlinx.android.synthetic.main.family_member_add_dialog.*
 import org.ascolto.onlus.geocrowd19.android.AscoltoActivity
+import org.ascolto.onlus.geocrowd19.android.AscoltoApplication
 import org.ascolto.onlus.geocrowd19.android.R
 import org.ascolto.onlus.geocrowd19.android.toast
+import org.ascolto.onlus.geocrowd19.android.ui.addrelative.AddRelativeActivity
 
 class FamilyDialogActivity: AscoltoActivity() {
 
@@ -24,7 +27,15 @@ class FamilyDialogActivity: AscoltoActivity() {
         }
 
         addMember.setOnClickListener {
-            toast("TODO go to add family member")
+            navigateToAddRelative()
+            finish()
         }
+    }
+
+    private fun navigateToAddRelative() {
+        val intent = Intent(AscoltoApplication.appContext, AddRelativeActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+        }
+        startActivity(intent)
     }
 }
