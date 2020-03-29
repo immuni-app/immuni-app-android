03ad15dc22480519cf1c92ab70f1cc506da683c3
Marco Uberti <marcouberti84@gmail.com>
2020-03-29 15:10:44 +0200

    fix: correct status bar theme in confirmation pages.

--- app/src/main/java/org/ascolto/onlus/geocrowd19/android/ui/uploadData/UploadDataSuccessFragment.kt
+++ app/src/main/java/org/ascolto/onlus/geocrowd19/android/ui/uploadData/UploadDataSuccessFragment.kt
@@ -3,10 +3,12 @@ package org.ascolto.onlus.geocrowd19.android.ui.uploadData
 import android.os.Bundle
 import android.text.InputFilter
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.bendingspoons.base.extensions.invisible
+import com.bendingspoons.base.extensions.setLightStatusBarFullscreen
 import com.bendingspoons.base.extensions.visible
 import kotlinx.android.synthetic.main.insert_upload_data_code_fragment.*
 import kotlinx.android.synthetic.main.where_to_find_the_code_fragment.*
@@ -26,6 +28,8 @@ class UploadDataSuccessFragment : Fragment(R.layout.upload_data_success_fragment
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        (activity as? AppCompatActivity)?.setLightStatusBarFullscreen(resources.getColor(android.R.color.transparent))
+
         viewModel = getSharedViewModel()
         CoroutineScope(Dispatchers.Main).launch {
             delay(2000)
