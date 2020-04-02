71a5210d5f756bb89fccd846b5d555252fa89ecc
Marco Uberti <marcouberti84@gmail.com>
2020-04-02 09:48:00 +0200

    chore: remove user db entity.

--- app/src/main/java/org/immuni/android/db/AscoltoDatabase.kt
+++ app/src/main/java/org/immuni/android/db/AscoltoDatabase.kt
@@ -6,15 +6,12 @@ import androidx.room.TypeConverters
 import org.immuni.android.db.converter.DateConverter
 import org.immuni.android.db.converter.GenderConverter
 import org.immuni.android.db.dao.BLEContactDao
-import org.immuni.android.db.dao.UserInfoDao
 import org.immuni.android.db.entity.BLEContactEntity
-import org.immuni.android.db.entity.UserInfoEntity
 
 const val DATABASE_VERSION = 3
 
 @Database(
     entities = [
-        UserInfoEntity::class,
         BLEContactEntity::class
     ],
     version = DATABASE_VERSION
