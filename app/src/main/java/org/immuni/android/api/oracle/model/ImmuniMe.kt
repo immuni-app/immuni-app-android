0c3ef06b58d89c62636da308e70c653bfe31cce4
Matteo Battaglio <m4dbat@gmail.com>
2020-04-15 17:06:02 +0200

    Add serverTriageProfileId setting

--- app/src/main/java/org/immuni/android/api/oracle/model/ImmuniMe.kt
+++ app/src/main/java/org/immuni/android/api/oracle/model/ImmuniMe.kt
@@ -1,11 +1,13 @@
 package org.immuni.android.api.oracle.model
 
 import com.bendingspoons.base.utils.JSonSerializable
-import org.immuni.android.models.User
 import com.bendingspoons.oracle.api.model.OracleMe
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
-import java.util.*
+import org.immuni.android.models.survey.TriageProfileId
+import org.immuni.android.models.survey.raw.RawTriageProfile
 
 @JsonClass(generateAdapter = true)
-class ImmuniMe: OracleMe(), JSonSerializable
+class ImmuniMe(
+    @field:Json(name = "server_triage") val serverTriageProfileId: TriageProfileId?
+): OracleMe(), JSonSerializable
