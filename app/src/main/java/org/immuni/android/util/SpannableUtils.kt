6844114f70732ea91ed9449249bb86566875c4d3
Marco Uberti <marcouberti84@gmail.com>
2020-04-19 11:12:56 +0200

    feat: add spannable util the bold option.

--- app/src/main/java/org/immuni/android/util/SpannableUtils.kt
+++ app/src/main/java/org/immuni/android/util/SpannableUtils.kt
@@ -1,10 +1,12 @@
 package org.immuni.android.util
 
+import android.graphics.Typeface
 import android.text.Spannable
 import android.text.SpannableString
 import android.text.style.ForegroundColorSpan
+import android.text.style.StyleSpan
 
-fun String.color(startPlaceholder: String, endPlaceholder: String, color: Int): Spannable {
+fun String.color(startPlaceholder: String, endPlaceholder: String, color: Int, bold: Boolean = false): Spannable {
     val textWithoutPlaceholders = this.replace(startPlaceholder, "")
         .replace(endPlaceholder, "")
     val start = this.indexOf(startPlaceholder)
@@ -15,5 +17,13 @@ fun String.color(startPlaceholder: String, endPlaceholder: String, color: Int): 
         start,
         end - 1,
         Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+    val boldStyle = StyleSpan(Typeface.BOLD)
+    spannable.setSpan(
+        boldStyle,
+        start,
+        end - 1,
+        Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
     return spannable
 }
