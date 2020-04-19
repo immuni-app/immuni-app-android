7eeaf8adcf0f5410c67a1fb9a11ed328499db553
Marco Uberti <marcouberti84@gmail.com>
2020-04-19 09:58:39 +0200

    feat: add unique id param in force update url.

--- app/src/main/java/org/immuni/android/ui/forceupdate/ForceUpdateViewModel.kt
+++ app/src/main/java/org/immuni/android/ui/forceupdate/ForceUpdateViewModel.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.lifecycle.ViewModel
 import com.bendingspoons.base.playstore.PlayStoreActions
 import com.bendingspoons.base.utils.ExternalLinksHelper
+import com.bendingspoons.concierge.ConciergeManager
 import com.bendingspoons.oracle.Oracle
 import org.immuni.android.api.oracle.model.ImmuniMe
 import org.immuni.android.api.oracle.model.ImmuniSettings
@@ -13,12 +14,15 @@ import org.koin.core.inject
 class ForceUpdateViewModel : ViewModel(), KoinComponent {
 
     val oracle: Oracle<ImmuniSettings, ImmuniMe> by inject()
+    val concierge: ConciergeManager by inject()
 
     fun goToPlayStoreAppDetails(context: Context) {
 
         // if we have a custon url
         oracle.settings()?.appUpdateUrl?.let {
-            ExternalLinksHelper.openLink(context, it)
+            ExternalLinksHelper.openLink(
+                context,
+                "${it}?id=${concierge.backupPersistentId.id}")
             return
         }
 
