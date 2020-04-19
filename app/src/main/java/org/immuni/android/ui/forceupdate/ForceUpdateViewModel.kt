5be5a340eb9d2a05138f45fe906081a0e2709b95
Marco Uberti <marcouberti84@gmail.com>
2020-04-19 12:39:34 +0200

    feat: use DownloadManager with user id header to download apk update.

--- app/src/main/java/org/immuni/android/ui/forceupdate/ForceUpdateViewModel.kt
+++ app/src/main/java/org/immuni/android/ui/forceupdate/ForceUpdateViewModel.kt
@@ -1,15 +1,20 @@
 package org.immuni.android.ui.forceupdate
 
+import android.app.DownloadManager
 import android.content.Context
+import android.net.Uri
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.lifecycle.ViewModel
 import com.bendingspoons.base.playstore.PlayStoreActions
 import com.bendingspoons.base.utils.ExternalLinksHelper
 import com.bendingspoons.concierge.ConciergeManager
 import com.bendingspoons.oracle.Oracle
+import org.immuni.android.R
 import org.immuni.android.api.oracle.model.ImmuniMe
 import org.immuni.android.api.oracle.model.ImmuniSettings
 import org.koin.core.KoinComponent
 import org.koin.core.inject
+
 
 class ForceUpdateViewModel : ViewModel(), KoinComponent {
 
@@ -19,14 +24,25 @@ class ForceUpdateViewModel : ViewModel(), KoinComponent {
     fun goToPlayStoreAppDetails(context: Context) {
 
         // if we have a custon url
-        oracle.settings()?.appUpdateUrl?.let {
-            ExternalLinksHelper.openLink(
-                context,
-                "${it}?id=${concierge.backupPersistentId.id}")
+        oracle.settings()?.appUpdateUrl?.let { url->
+            startDownload(context, url)
             return
         }
 
         // otherwise go to play store
         PlayStoreActions.goToPlayStoreAppDetails(context)
+    }
+
+    private fun startDownload(context: Context, url: String) {
+        val downloadManager = context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+        val uri: Uri = Uri.parse(url)
+        val request = DownloadManager.Request(uri).apply {
+            setTitle(context.getString(R.string.immuni_update_file_title))
+            setDescription(context.getString(R.string.immuni_update_file_description))
+            setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+            addRequestHeader("device-id", concierge.backupPersistentId.id)
+        }
+
+        downloadManager.enqueue(request)
     }
 }
