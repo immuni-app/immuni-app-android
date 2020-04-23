3163141ce4474c38fcf97eae7824e75c41147c03
Matteo Battaglio <m4dbat@gmail.com>
2020-04-23 14:53:40 +0200

    Add overloads of toJson and fromJson accepting a vararg list of adapters

--- app/src/main/java/org/immuni/android/util/FlagsManager.kt
+++ app/src/main/java/org/immuni/android/util/FlagsManager.kt
@@ -2,6 +2,7 @@ package org.immuni.android.util
 
 import android.content.Context
 import androidx.core.content.edit
+import com.squareup.moshi.JsonAdapter
 import org.immuni.android.ImmuniApplication
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.adapters.Rfc3339DateJsonAdapter
@@ -13,15 +14,21 @@ object Flags {
 }
 
 fun isFlagSet(flag: String): Boolean {
-    val sharedPrefs = ImmuniApplication.appContext.getSharedPreferences("IMMUNI_FLAGS", Context.MODE_PRIVATE)
+    val sharedPrefs = ImmuniApplication.appContext.getSharedPreferences(
+        "IMMUNI_FLAGS",
+        Context.MODE_PRIVATE
+    )
     return sharedPrefs.getBoolean(flag, false)
 }
 
 fun setFlag(flag: String, value: Boolean = true) {
-    val sharedPrefs = ImmuniApplication.appContext.getSharedPreferences("IMMUNI_FLAGS", Context.MODE_PRIVATE)
+    val sharedPrefs = ImmuniApplication.appContext.getSharedPreferences(
+        "IMMUNI_FLAGS",
+        Context.MODE_PRIVATE
+    )
     sharedPrefs.edit {
-            putBoolean(flag, value)
-        }
+        putBoolean(flag, value)
+    }
 }
 
 inline fun <reified T : Any> toJson(obj: T): String {
@@ -33,8 +40,26 @@ inline fun <reified T : Any> toJson(obj: T): String {
     return jsonAdapter.toJson(obj)
 }
 
+inline fun <reified T : Any, K> toJson(obj: T, vararg adapters: JsonAdapter<K>): String {
+    val moshi = adapters.fold(Moshi.Builder()) { builder, adapter -> builder.add(adapter) }
+        .add(KotlinJsonAdapterFactory())
+        .add(Date::class.java, Rfc3339DateJsonAdapter().nullSafe())
+        .build()
+    val jsonAdapter = moshi.adapter(T::class.java)
+    return jsonAdapter.toJson(obj)
+}
+
 inline fun <reified T : Any> fromJson(json: String): T? {
     val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .add(Date::class.java, Rfc3339DateJsonAdapter().nullSafe())
+        .build()
+    val jsonAdapter = moshi.adapter(T::class.java)
+    return jsonAdapter.fromJson(json)
+}
+
+inline fun <reified T : Any, K> fromJson(json: String, vararg adapters: JsonAdapter<K>): T? {
+    val moshi = adapters.fold(Moshi.Builder()) { builder, adapter -> builder.add(adapter) }
         .add(KotlinJsonAdapterFactory())
         .add(Date::class.java, Rfc3339DateJsonAdapter().nullSafe())
         .build()
