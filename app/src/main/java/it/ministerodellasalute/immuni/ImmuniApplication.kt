9b7ac9f70fbad5bf50662bcc00a3c18ce1e29302
Marco Uberti <marcouberti84@gmail.com>
2020-06-04 22:58:34 +0200

    feat: risky contact suggestions copy update (#180)
    * feat: add deactivate service button
    
    * fix: correct button copy
    
    * fix: add dot in copy
    
    * feat: add disable exposure api dialog
    
    * fix: string keys
    
    * fix: layouts
    
    * feat: disable exposure notifications logic
    
    * chore: ktlint
    
    * feat: add footer copy in risky contact dialog
    
    Co-authored-by: Ioane Sharvadze <is@bendingspoons.com>

--- app/src/main/java/it/ministerodellasalute/immuni/ImmuniApplication.kt
+++ app/src/main/java/it/ministerodellasalute/immuni/ImmuniApplication.kt
@@ -38,7 +38,6 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.KoinComponent
 import org.koin.core.context.startKoin
-import org.koin.core.inject
 
 class ImmuniApplication : Application(), KoinComponent {
 
