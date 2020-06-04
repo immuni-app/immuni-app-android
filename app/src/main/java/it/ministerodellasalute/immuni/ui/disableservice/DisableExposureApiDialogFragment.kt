da18934948513118111b73ca344afbce76b5cf57
Marco Uberti <marcouberti84@gmail.com>
2020-06-04 21:46:00 +0200

    feat: disable exposure notifications logic

--- app/src/main/java/it/ministerodellasalute/immuni/ui/disableservice/DisableExposureApiDialogFragment.kt
+++ app/src/main/java/it/ministerodellasalute/immuni/ui/disableservice/DisableExposureApiDialogFragment.kt
@@ -2,10 +2,15 @@ package it.ministerodellasalute.immuni.ui.disableservice
 
 import android.os.Bundle
 import android.view.View
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import it.ministerodellasalute.immuni.R
 import it.ministerodellasalute.immuni.extensions.view.setSafeOnClickListener
+import it.ministerodellasalute.immuni.logic.exposure.ExposureManager
 import it.ministerodellasalute.immuni.ui.dialog.PopupDialogFragment
 import kotlinx.android.synthetic.main.disable_exposure_api_dialog.*
+import kotlinx.coroutines.launch
+import org.koin.android.ext.android.inject
 
 class DisableExposureApiDialogFragment : PopupDialogFragment() {
 
@@ -16,7 +21,11 @@ class DisableExposureApiDialogFragment : PopupDialogFragment() {
         setTitle(getString(R.string.permission_tutorial_deactivate_service_title))
 
         disableExposureApi.setSafeOnClickListener {
-            // TODO
+            val exposureManager: ExposureManager by inject()
+            lifecycleScope.launch {
+                exposureManager.stopExposureNotification()
+                findNavController().popBackStack()
+            }
         }
     }
 }
