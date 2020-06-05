17c2cadabb5ec11babc93ba045abb27a330cc005
enricocid <enrico2588@gmail.com>
2020-06-05 22:46:25 +0200

    feat/proposal: open external links within the app
    Signed-off-by: enricocid <enrico2588@gmail.com>

--- extensions/src/main/java/it/ministerodellasalute/immuni/extensions/utils/ExternalLinksHelper.kt
+++ extensions/src/main/java/it/ministerodellasalute/immuni/extensions/utils/ExternalLinksHelper.kt
@@ -19,6 +19,7 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.widget.Toast
+import androidx.browser.customtabs.CustomTabsIntent
 
 object ExternalLinksHelper {
 
@@ -29,15 +30,26 @@ object ExternalLinksHelper {
      * @param errorMessage, to be shown if it impossible to open the link with any app.
      */
     fun openLink(context: Context, url: String, errorMessage: String = "Cannot open the link.") {
-        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-        browserIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        val customTabsIntent = CustomTabsIntent.Builder()
+            .addDefaultShareMenuItem()
+            .setShowTitle(true)
+            .build()
 
+        val parsedUri = Uri.parse(url)
         val manager = context.packageManager
-        val infos = manager.queryIntentActivities(browserIntent, 0)
+        val infos = manager.queryIntentActivities(customTabsIntent.intent, 0)
         if (infos.size > 0) {
-            context.startActivity(browserIntent)
+            customTabsIntent.launchUrl(context, parsedUri)
         } else {
-            Toast.makeText(context, errorMessage, Toast.LENGTH_SHORT).show()
+            val browserIntent = Intent(Intent.ACTION_VIEW, parsedUri)
+            browserIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+
+            val fallbackInfos = manager.queryIntentActivities(browserIntent, 0)
+            if (fallbackInfos.size > 0) {
+                context.startActivity(browserIntent)
+            } else {
+                Toast.makeText(context, errorMessage, Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
