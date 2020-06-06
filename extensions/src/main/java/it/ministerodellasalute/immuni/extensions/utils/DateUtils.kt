00e8370e8439131000c10289b8ffd86f966edb09
Matteo Battaglio <mab@bendingspoons.com>
2020-06-06 02:22:34 +0200

    feat: add `byAdding` utility method to Date

--- extensions/src/main/java/it/ministerodellasalute/immuni/extensions/utils/DateUtils.kt
+++ extensions/src/main/java/it/ministerodellasalute/immuni/extensions/utils/DateUtils.kt
@@ -26,3 +26,32 @@ object DateUtils {
         Locale.US
     ).parse(date)
 }
+
+fun Date.byAdding(
+    days: Int = 0,
+    hours: Int = 0,
+    minutes: Int = 0,
+    seconds: Int = 0,
+    milliseconds: Int = 0,
+    calendar: Calendar = Calendar.getInstance()
+): Date {
+    val date = this
+    return calendar.apply {
+        time = date
+        if (days != 0) {
+            add(Calendar.DAY_OF_YEAR, days)
+        }
+        if (hours != 0) {
+            add(Calendar.HOUR_OF_DAY, hours)
+        }
+        if (minutes != 0) {
+            add(Calendar.MINUTE, minutes)
+        }
+        if (seconds != 0) {
+            add(Calendar.SECOND, seconds)
+        }
+        if (milliseconds != 0) {
+            add(Calendar.MILLISECOND, milliseconds)
+        }
+    }.time
+}
